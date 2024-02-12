5fb751f59ffd82b0c92af029b3e87bfcdc32384a
saurav kunwor <kunworsaurav3@gmail.com>
2024-02-12 12:44:31 +0545

    fist commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const mylabel = document.getElementById("mylabel");
+
+update();
+setInterval(update, 1000);
+
+function update() {
+    let date = new Date();
+    mylabel.innerHTML = formateTime(date);
+
+    function formateTime(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let amOrPm = hours >= 12 ? "pm" : "am";
+
+        hours = (hours % 12) || 12;
+
+        hours = formatZeroes(hours);
+        minutes = formatZeroes(minutes);
+        seconds = formatZeroes(seconds);
+
+        return `${hours}:${minutes}:${seconds}:${amOrPm}`;
+    }
+    function formatZeroes(time) {
+        time = time.toString();
+        return time.length < 2 ? "0" + time : time;
+    }
+}
